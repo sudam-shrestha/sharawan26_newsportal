2808374566171c4dfc665c8824a793359b5c1ae6
sudam-shrestha <sudamshrestha939@gmail.com>
2025-08-20 20:49:37 +0545

    admin dashboard setup

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,25 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="{{ route('dashboard') }}">
+            SUDAM HUB
+        </a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="briefcase"></i><span>Company</span></a>
+        </li>
+
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="tag"></i><span>Category</span></a>
+        </li>
+
+        <li class="dropdown">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="folder"></i><span>Article</span></a>
+        </li>
+    </ul>
+</aside>
